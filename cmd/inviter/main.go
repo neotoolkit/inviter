9f1976322b9b6a20674ad7b91bd3d3b952a804ca
Sasha Melentyev <sasha@melentyev.io>
2022-02-02 17:43:27 +0300

    feat: add concept

--- cmd/inviter/main.go
+++ cmd/inviter/main.go
@@ -1,3 +1,24 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/google/go-github/v42/github"
+	"golang.org/x/oauth2"
+)
+
+func main() {
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: os.Getenv("GITHUB_ACCESS_TOKEN")},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+
+	_, _, err := client.Teams.AddTeamMembershipBySlug(ctx, "neotoolkit", "team", "neotoolkit-bot", &github.TeamAddTeamMembershipOptions{})
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
